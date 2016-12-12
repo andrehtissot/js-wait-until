8f9325bb677b296d18d852cd6a7da7366113fc0a
Andre Augusto Tissot <andre_a_tissot@yahoo.com.br>
2016-12-12 16:27:50 -0200

    First commit

---
+++ waitUntil.js
@@ -0,0 +1,23 @@
+/*!
+ * Generic wait until something happen function v1.0
+ * https://github.com/andrehtissot/js-wait-until
+ *
+ * Copyright André Augusto Tissot
+ * Released under the MIT license
+ *
+ * Date: 2016-12-12
+ */
+var waitUntil = function(testFunction, tries, intervalMiliseconds){
+  var tries = tries || -1;
+  return new Promise(function(resolve, reject){
+    var intervalId = setInterval(function(){
+      if(testFunction()) {
+        clearInterval(intervalId);
+        resolve();
+      } else if(--tries === 0) {
+        clearInterval(intervalId);
+        reject();
+      }
+    }, intervalMiliseconds || 50);
+  });
+}
